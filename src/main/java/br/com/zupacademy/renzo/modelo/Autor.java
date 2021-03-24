2e3bfb47d5b25a63c47c79cfeb0a16827d48ac5e
Renzo Marquez Piedade <renzo.piedade@zup.com.br>
2021-03-24 18:42:59 -0300

    Adicionou modelo de Autor e Application properties.

---
+++ src/main/java/br/com/zupacademy/renzo/modelo/Autor.java
@@ -0,0 +1,79 @@
+package br.com.zupacademy.renzo.modelo;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Autor {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private String nome;
+	@Column(nullable = false)
+	private String email;
+	@Column(nullable = false,length = 400)
+	private String descricao;
+	@Column(nullable = false)
+	private LocalDateTime dataRegistro = LocalDateTime.now();
+	
+	
+
+	public Autor() {}
+	
+	public Autor(String nome, String email, String descricao) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.descricao = descricao;
+	}
+	public Long getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public LocalDateTime getDataRegistro() {
+		return dataRegistro;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public void setDataRegistro(LocalDateTime dataRegistro) {
+		this.dataRegistro = dataRegistro;
+	}
+
+	@Override
+	public String toString() {
+		return "Autor [nome=" + nome + ", email=" + email + ", descricao=" + descricao + ", dataRegistro="
+				+ dataRegistro + "]";
+	}
+	
+	
+	
+}
